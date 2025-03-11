b72ffb7968b27ba44d6fd732393eb12faca62755
Captain Mirage <chargersrthc6.2@gmail.com>
2025-03-11 18:10:45 +0330

    System update: help command args #1.1

--- src/shell/types.rs
+++ src/shell/types.rs
@@ -4,5 +4,6 @@ use std::collections::{HashMap, HashSet};
 #[derive(Debug, Clone, Serialize)]
 pub struct CommandInfo {
     pub title: &'static str,
+    pub cmd_args: &'static str,
     pub description: &'static str,
 }
