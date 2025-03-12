d95e3a8b079b328bf92ef98ec54fce05e00e6d52
CaptainMirage <87281406+CaptainMirage@users.noreply.github.com>
2025-03-12 09:42:48 +0330

    Format update: fmt formatting #1.4

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,16 @@
-mod analyzer;
+mod analyser;
 mod shell;
-// use crate::analyzer::utils::tester_function;
-use crate::analyzer::{
+// use crate::Analyser::utils::tester_function;
+use crate::analyser::{
     constants::*,
     // utils::{type_text}
 };
 
-
 #[cfg(feature = "DEBUG_MODE")]
 fn debug_test() -> std::io::Result<()> {
-    let mut analyzer = StorageAnalyzer::new();
-    analyzer.print_recent_large_files("C:\\")?;
-    analyzer.print_old_large_files("C:\\")?;
+    let mut Analyser = StorageAnalyser::new();
+    Analyser.print_recent_large_files("C:\\")?;
+    Analyser.print_old_large_files("C:\\")?;
     Ok(())
 }
 
@@ -29,7 +28,7 @@ fn main() -> std::io::Result<()> {
         println!("DEBUG MODE : Running debug function!");
         return debug_test();
     }
-    
+
     // where the main code will run
 
     shell::bash_commands();
