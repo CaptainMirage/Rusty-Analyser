ea40b7c45a8c5fceeb97b3328292636b5008a3fa
Captain Mirage <chargersrthc6.2@gmail.com>
2025-05-05 19:58:14 +0330

    System Update : minor bug fix #1

--- src/main.rs
+++ src/main.rs
@@ -10,14 +10,14 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         println!("if you are a normal user, consider using cargo run --release\n\n\n");
     }
 
-    let flag: bool = false;
+    let flag: bool = true;
     if flag {
         // where the main code will run
         display_boot_sequence();
 
-        shell::bash_commands();
+        shell::ntfs_bash_commands();
     }
-    shell::ntfs_bash_commands();
+    // shell::ntfs_bash_commands();
 
     Ok(())
 }
